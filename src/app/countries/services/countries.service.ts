b95d118da4d3ee0d01b3396cad825e183e3ea58e
rramirez <ro.analisis@egmail.com>
2024-05-11 11:44:21 -0400

    persistencia en pantalla

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, delay, map, of } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country';
+import { CacheStore } from '../interfaces/cache-store.interface';
+import { Region } from '../interfaces/region.type';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
 
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
-  constructor(private http: HttpClient) { }
+  public cacheStore: CacheStore = {
+    byCapital: { term: '', countries: [] },
+    byCountries: { term: '', countries: [] },
+    byRegion: { region: '', countries: [] }
+  }
+
+  constructor(private http: HttpClient) {}
 
   private getCountriesRequest( url: string ): Observable<Country[]> {
     return this.http.get<Country[]>( url )
     .pipe(
       catchError( () => of([]) ),
-      delay( 2000 ),
+      // delay( 2000 ),
     );
   }
 
@@ -31,17 +39,26 @@ export class CountriesService {
 
   searchCapital( term: string ): Observable<Country[]> {
     const url = `${ this.apiUrl }/capital/${ term }`;
-    return this.getCountriesRequest( url );
+    return this.getCountriesRequest( url )
+    .pipe(
+      tap( countries => this.cacheStore.byCapital = { term, countries })
+    );
   }
 
   searchCountry( term: string ): Observable<Country[]> {
     const url = `${ this.apiUrl }/name/${ term }`;
-    return this.getCountriesRequest( url );
+    return this.getCountriesRequest( url )
+    .pipe(
+      tap( countries => this.cacheStore.byCountries = { term, countries })
+    );
   }
 
-  searchRegion( term: string ): Observable<Country[]> {
-    const url = `${ this.apiUrl }/region/${ term }`;
-    return this.getCountriesRequest( url );
+  searchRegion( region: Region ): Observable<Country[]> {
+    const url = `${ this.apiUrl }/region/${ region }`;
+    return this.getCountriesRequest( url )
+    .pipe(
+      tap( countries => this.cacheStore.byRegion = { region, countries })
+    );
   }
 
 }
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,20 +1,48 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit, OnDestroy {
+
+  private debouncer: Subject<string> = new Subject<string>();
+  private debounceSubscription?: Subscription;
 
   @Input()
   public placeholder: string = '';
 
+  @Input()
+  public initialValue: string = '';
+
   @Output()
   public onValue = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debounceSubscription = this.debouncer
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe( value => {
+      this.onDebounce.emit( value );
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.debounceSubscription?.unsubscribe();
+  }
+
   emitValue( value: string ):void {
     this.onValue.emit( value );
+  }
+
+  onKeyPress( searchTerm: string ) {
+    this.debouncer.next( searchTerm )
   }
 
 
